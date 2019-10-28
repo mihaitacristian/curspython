55e2c41cad43412259eff121b66df0181354b1d1
Manda <mihaita-cristian.manda@atos.net>
2019-10-28 18:52:13 +0200

    add bubble_sort example

---
+++ Lessons/bubble_sort.py
@@ -0,0 +1,14 @@
+lista = []
+swapped = True
+num = int(input("Cate elemente doriti sa aiba lista ?:"))
+for i in range (num):
+    val = int(input("Introduceti un element:"))
+    lista.append(val)
+while swapped:
+    swapped=False
+    for i in range (len(lista)-1):
+        if lista[i]>lista[i+1]:
+            swapped=True
+            lista[i],lista[i+1]=lista[i+1],lista[i]
+print("Lista sortata:")
+print(lista)
