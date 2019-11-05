bbe01b30075a07baad27fbcbec861f955533a5fa
Manda <mihaita-cristian.manda@atos.net>
2019-11-05 09:02:15 +0200

    add ex

---
+++ Lessons/mod2_ex.py
@@ -0,0 +1,14 @@
+# print('"Hello ",', '"007"')
+# print('"1.5"',2.0,528,False,sep=",")
+# print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+# print((2 ** 4), (2 * 4.), (2 * 4))
+# print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+# var=100
+# var+=1 # var = var + 1 
+# print (var)
+# anything = int(input("Enter a number: "))
+# something = anything ** 2.0
+# print(anything, "to the power of 2 is", something)
+# print(("|" + " " * 10 + "|\n") * 5,end="")
+keyword=1
+print(keyword)
