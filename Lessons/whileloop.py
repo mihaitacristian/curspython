a263476e262f0ffbf0e5a7cf99b51e8471ca6885
Manda <mihaita-cristian.manda@atos.net>
2019-11-18 01:10:37 +0200

    .

--- Lessons/whileloop.py
+++ Lessons/whileloop.py
@@ -20,20 +20,20 @@
 #     print ("You win!")    
 
 
-secret_number = 10
-concurent = int(input("Ghiceste numarul corect ...: "))
-secret_conter = 0
-secret_limit = 10
-out_of_luck = False
+# secret_number = 10
+# concurent = int(input("Ghiceste numarul corect ...: "))
+# secret_conter = 0
+# secret_limit = 10
+# out_of_luck = False
 
-while concurent != secret_number and not out_of_luck:
-    if secret_conter<secret_limit:
-        concurent = int(input("Ghiceste numarul corect ...: "))
-        secret_conter += 1
-    else:
-        out_of_luck=True
+# while concurent != secret_number and not out_of_luck:
+#     if secret_conter<secret_limit:
+#         concurent = int(input("Ghiceste numarul corect ...: "))
+#         secret_conter += 1
+#     else:
+#         out_of_luck=True
 
-if out_of_luck:
-    print("Keept trying....!")
-else:
-    print("you found the secret")    
+# if out_of_luck:
+#     print("Keept trying....!")
+# else:
+#     print("you found the secret")    
